c0709718f0d82f8d2f1bb1cb042ffcebb63d2e81
Julian Guarin <julian.guarin@fitech.com.co>
2018-01-08 06:26:11 -0500

    InsertionSorter as templated library

--- insertion/insertion.cpp
+++ insertion/insertion.cpp
@@ -1,41 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <sort/sort.h>
-using namespace std; 
-
-
-template <typename T>
-class InsertionSorter : public Sorter<T>
-{
-	void TraverseAndCompare(vector<T>& u, long size, long pivot)
-	{
-		auto data = u.data();
-		for (auto index = pivot; index > 0 && !this->IsSortedSequel(u,index-1,index); --index)
-        {
-        	swap(data[index-1], data[index]);
-        	 ++this->m_count;
-        }
-	}
-public:
-	InsertionSorter():Sorter<T>(){
-	}
-
-	void Sort(vector<T>& u) override {
-        this->ResetCounter();
-        auto size = static_cast<long>(u.size());
-        for (auto pivot = 1; pivot < size - 1; ++pivot)
-		{
-			this->Print(u);
-			this->TraverseAndCompare(u, size, pivot);
-		} 
-		this->Print(u);
-        cout << "** " << this->m_count << " **\n";
-	}
-};
+#include "insertion.h" 
 
 
 
 
+
+using namespace std;
 int main() 
 {
 	vector<int>data{55,-2,34,10,0,2,-5,12};
---
+++ insertion/insertion.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <sort/sort.h>
+
+template <typename T>
+class InsertionSorter : public Sorter<T>
+{
+	void TraverseAndCompare(std::vector<T>& u, long size, long pivot)
+	{
+		auto data = u.data();
+		for (auto index = pivot; index > 0 && !this->IsSortedSequel(u,index-1,index); --index)
+        {
+        	std::swap(data[index-1], data[index]);
+        	++this->m_count;
+        }
+	}
+public:
+	InsertionSorter():Sorter<T>(){
+	}
+
+	void Sort(std::vector<T>& u) override {
+        this->ResetCounter();
+        auto size = static_cast<long>(u.size());
+        for (auto pivot = 1; pivot < size - 1; ++pivot)
+		{
+			this->Print(u);
+			this->TraverseAndCompare(u, size, pivot);
+		} 
+		this->Print(u);
+        std::cout << "** " << this->m_count << " **\n";
+	}
+};
