e7be1edcd496568ff375e1718d75223ef20cd0bf
Julian Guarin <julian.guarin@fitech.com.co>
2018-01-31 00:44:50 -0500

    Quick Sort enhanced

--- quick/quick.h
+++ quick/quick.h
@@ -9,66 +9,39 @@ class QuickSorter : public Sorter<T>
 {
     unsigned int m_pivotalIndex = 0;
 
-    void SortInplaceVector(std::vector<T>& unorderedVector, int from, int to, int pivotIndex = 0){
-        
-        auto begin = from;
-        auto end = to;
+    void SortInplaceVector(std::vector<T>& unorderedVector, int from, int to){
         
         //Ordered by size definition.
-        if (end - begin < 2)
+        if (to - from < 2)
         {
-            if (unorderedVector[begin] > unorderedVector[end]) std::swap(unorderedVector[begin], unorderedVector[end]);
-            this->Print(unorderedVector);
+            if (unorderedVector[from] > unorderedVector[to]) std::swap(unorderedVector[from], unorderedVector[to]);
+            this -> Print(unorderedVector);
             return;
         }
-
-        //Set up the partition
-        this->Print(unorderedVector);
-        std::swap(unorderedVector[end], unorderedVector[pivotIndex]);
-        this->Print(unorderedVector);
-        pivotIndex = end;
-        end--;
-
+        
+        auto pivotIndex = from + to;
+        pivotIndex >>= 1;
+        
         auto pivot = unorderedVector[pivotIndex];
         
-        while (begin <= end){
+        //Set up the partition
+        std::swap(unorderedVector[pivotIndex], unorderedVector[to]);
+        this -> Print(unorderedVector);
+        pivotIndex = from;
+        for (auto index = from; index < to; ++index){
             
-            auto hi = unorderedVector[end];
-            auto li = unorderedVector[begin];
-            
-            if (hi > pivot && li <= pivot){
-                end--; begin++; continue;
-            } else if ( hi > pivot ) {
-                end--;
-            } else if ( li <= pivot ) {
-                begin++;
-            } else {
-                std::swap(unorderedVector[begin++], unorderedVector[end--]);
-                this->Print(unorderedVector);
-            }
+            if(unorderedVector[index] <= pivot) {
+                std::swap(unorderedVector[index], unorderedVector[pivotIndex++]);
+                this -> Print(unorderedVector);
+            }            
         }
-        
-        if ( begin == pivotIndex) SortInplaceVector(unorderedVector, from, pivotIndex - 1, (from + pivotIndex - 1) >> 1);
-        
-        else if ( end == from - 1){
-            
-            this->Print(unorderedVector);
-            std::swap(unorderedVector[from], unorderedVector[pivotIndex]);
-            this->Print(unorderedVector);
-            SortInplaceVector(unorderedVector, from + 1, pivotIndex, (from + 1 + pivotIndex) >> 1);
-        
-        } else {
+        std::swap(unorderedVector[pivotIndex], unorderedVector[to]);
+        this -> Print(unorderedVector);
 
-            std::swap(begin, end);
-            this->Print(unorderedVector);
-            std::swap(unorderedVector[pivotIndex], unorderedVector[end]);
-            this->Print(unorderedVector);
-
-            SortInplaceVector(unorderedVector, from, begin, (from + begin) >> 1 );
-            SortInplaceVector(unorderedVector, end + 1, pivotIndex, (end + 1 + pivotIndex) >> 1);
-
-        }  
-
+        //Swap Partitions
+        SortInplaceVector(unorderedVector, from, pivotIndex - 1);
+        SortInplaceVector(unorderedVector, pivotIndex + 1, to);
+                   
     }
     
 public:
@@ -76,7 +49,7 @@ public:
 	void Sort(std::vector<T>& u)
 	{
         auto sizeIndex = u.size()-1;
-		SortInplaceVector(u, 0, sizeIndex, sizeIndex>>1);
+		SortInplaceVector(u, 0, sizeIndex);
 	}
 
 };
