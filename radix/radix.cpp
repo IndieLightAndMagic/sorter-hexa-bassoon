e95d733d2f33339e2a614384aa17db3033a13bf8
IndieLightAndMagic <julian.guarin.2010@gmail.com>
2018-03-09 23:04:10 -0500

    Radix Sort

--- radix/radix.cpp
+++ radix/radix.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main(){
-	vector<int>data{34, -3, 0, 2, 14, 8, -1, 24};
-	RadixSorter<int>sorter;
+	vector<unsigned int>data{34, 3, 0, 2, 14, 8, 1, 24};
+	RadixSorter<unsigned int>sorter;
 	sorter.Sort(data);
 	sorter.Print(data);
 	return 0;
--- radix/radix.h
+++ radix/radix.h
@@ -1,24 +1,47 @@
 #ifndef __HEAP_H__
 #define __HEAP_H__
 
+#include <cmath>
 #include <array>
 #include <algorithm>
 #include <sort/sort.h>
 
 template <typename T>
 
-class RadixSorter: public Sorter<int> {
+class RadixSorter: public Sorter<T> {
 
 	void RadixSort(std::vector<unsigned int>& u){
 
-        std::array<std::vector<unsigned int>
+        std::array<std::vector<unsigned int>, 10> count;
+        auto shift = 0;
+        auto allzero = false;
 
+        while (!allzero){
+        	
+        	allzero = true;
+        	auto index = 0;
+        	for (auto x:u){
+        		
+        		auto quotient = static_cast<int>(pow(10.0,shift));
+        		count[(x/quotient)%10].push_back(x);
+        		if (x/quotient) allzero = false;
+
+        	}
+        	shift++;
+
+        	for (auto& v:count){
+        		for (auto elem: v){
+        			u[index++] = elem;
+        		}
+				v.clear();
+        	}
+        }
 	}
 
 public:
 
 	RadixSorter(){}
-	void Sort(std::vector<int>&u){
+	void Sort(std::vector<unsigned int>&u){
 		RadixSort(u);
 	}
 };
